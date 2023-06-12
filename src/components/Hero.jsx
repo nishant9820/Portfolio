4ab8a1e773732524af9673b42d1b687660e46303
Nishant Desai <you@example.com>
2023-06-12 21:56:32 +0530

    f  commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -46,7 +46,7 @@ const Hero = () => {
           className="absolute  bottom-72 w-full
         flex justify-center  items-center"
         >
-          <img src={pc} alt="PC" className="w-screen  h-screen" />
+          <img src={pc} alt="PC" className="w-full h-full" />
         </div>
       ) : (
         <ComputersCanvas />
