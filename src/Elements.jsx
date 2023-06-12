2f024861cdab8b0884352a347f66f99513d33fb7
Nishant Desai <you@example.com>
2023-06-12 19:56:49 +0530

    24 commit

--- src/Elements.jsx
+++ src/Elements.jsx
@@ -13,7 +13,7 @@ import {
 const Elements = () => {
   return (
     <div className="relative z-0 bg-primary">
-      <div className=" bg-cover bg-no-repeat bg-center">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
         <Navbar />
         <Hero />
       </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      <ComputersCanvas />
+      {/* <ComputersCanvas /> */}
       <div
         className="absolute  xs:bottom-5 bottom-32 w-full
        flex justify-center items-center"
